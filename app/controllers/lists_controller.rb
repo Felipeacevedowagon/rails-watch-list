b9b5c45406089e47ad02c0241fe645df5226d343
Felipe Acevedo <pfacevedo@misena.edu.co>
2022-11-24 16:39:55 -0500

    day one

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,14 @@
 class ListsController < ApplicationController
   def index
+    @movies = Movie.all
   end
+
+  def show
+   @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # root "articles#index"
-  # get '/bookmarks/new' to: 'bookmarks#new'
-  # post '/bookmarks', to: 'boomarks#create'
-  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
-  resources :lists #, except: [:destroy, :edit, :update]
+  resources :lists, except: %i[destroy edit updated]
 end
