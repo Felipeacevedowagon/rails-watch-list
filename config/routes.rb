142f9c8efffb5091fc5aee0f6646b7cdad34f346
Felipe Acevedo <pfacevedo@misena.edu.co>
2022-11-24 15:01:14 -0500

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/bookmarks/new' to: 'bookmarks#new'
+  # post '/bookmarks', to: 'boomarks#create'
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
+  resources :lists #, except: [:destroy, :edit, :update]
 end
