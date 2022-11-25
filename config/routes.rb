26c07a9ffeae816e41391dfbc718197748d0a226
Felipe Acevedo <pfacevedo@misena.edu.co>
2022-11-25 13:37:57 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  resources :lists, except: %i[destroy edit updated]
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
