8ea65997e80f276a8385799f13b11de6fb64ffbb
Felipe Acevedo <pfacevedo@misena.edu.co>
2022-11-25 16:54:34 -0500

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,35 @@
 class ListsController < ApplicationController
   def index
-    @movies = Movie.all
+    @lists = List.all
   end
 
   def show
-   @movie = Movie.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def new
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @list = list.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
